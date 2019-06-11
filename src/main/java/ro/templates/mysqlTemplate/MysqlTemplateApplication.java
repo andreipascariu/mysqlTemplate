a44a65b09d5fd67ff6953da63222cffbf1b86b7d
Andrei <andrei.pas22@gmail.com>
2019-06-11 18:29:13 +0300

    Initial commit

---
+++ src/main/java/ro/templates/mysqlTemplate/MysqlTemplateApplication.java
@@ -0,0 +1,13 @@
+package ro.templates.mysqlTemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysqlTemplateApplication.class, args);
+	}
+
+}
