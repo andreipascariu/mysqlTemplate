f80dfa4d883232ae8d8a0fb1ac00f08fdce9efef
Andrei <andrei.pas22@gmail.com>
2019-07-29 14:29:40 +0300

    configs

--- src/main/java/ro/templates/mysqlTemplate/utils/Configuration.java
+++ src/main/java/ro/templates/mysqlTemplate/utils/Configuration.java
@@ -1,4 +1,11 @@
 package ro.templates.mysqlTemplate.utils;
 
 public class Configuration {
+
+    public static final String DATABASE_URL = "url";
+    public static final int DATABASE_PORT = 3306;
+    public static final String DATABASE_NAME = "mySQLTemplate";
+    public static final String DATABASE_USER = "root";
+    public static final String DATABASE_PASSWORD = "mysqlpassword";
+
 }
